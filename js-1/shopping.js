c0f8c02ffdcffe7ee6f68db3596b76b57bc3c086
SakshiKhatod <sakshi.khatod1998@gmail.com>
2021-03-07 22:08:59 +0530

    Complete code shopping.js

--- js-1/shopping.js
+++ js-1/shopping.js
@@ -1,3 +1,4 @@
+
 const sampleData = [
   ['apple', 'Fruit', 'isEdible', 40],
   ['Lays', 'Processed Food', 'isEdible', 10],
@@ -7,26 +8,19 @@ const sampleData = [
   ['Peanut Butter', 'Processed Food', 'isEdible', 200],
   ['Oven', 'Appliance', 'isNotEdible', 1300],
   ['Air Conditioner', 'Applicance', 'isNotEdible', 13300],
-  ['Drill', 'Power Tool', 'isNotEdible', 2200]
+  ['Drill', 'Power Tool', 'isNotEdible', 2200],
 ];
 
 // Create a list of items
 // result = [ { name: 'apple', category: 'Fruit', isEdible: true, price: 40 }, ... ]
-function createListOfObjects(sampleData) {
-  
-    var obj={};
-    for(var i=0;i<sampleData.length;i++)
-    {
-    obj['name']=sampleData[i][0];
-    obj['category']=sampleData[i][1];
-    obj['isEdible']=sampleData[i][2];
-    obj['price']=sampleData[i][3];
-    console.log(obj);
-    }
- // return obj;
+function createListOfObjects(data) {
 
+  const list_obj=data.map(entry=> { const [name,category,edibility,price]=entry;
+    return{
+      name,category,isEdible: edibility==='isEdible',price,
+    };});
+  return list_obj;
 }
-createListOfObjects(sampleData);
 
 /* sort by given field. Sorting field eg: 'category' | 'price' etc
 * sample input: createListOfObjects(sampleData), 'price'
@@ -41,7 +35,12 @@ createListOfObjects(sampleData);
 *   ...
 * ]
 */ 
-function sortListOfObjectsByField(objList, field) {}
+function sortListOfObjectsByField(objList, field) {
+  const result = objList.sort((a, b) => (a[field] > b[field]) ? 1 : (a[field] < b[field]) ? -1 : 0);
+  return result;
+
+  
+}
 
 /* return list of items of given category eg: 'Fruit' | 'Appliance'
  * sample input: objList, 'Fruit'
@@ -51,8 +50,19 @@ function sortListOfObjectsByField(objList, field) {}
  *    { name: 'Banana', category: 'Fruit', isEdible: true, price: 14 },
  * ]
  */
-function selectByCatergory(category) {}
+function selectByCatergory(list,field) {
+  const result = list.filter(entry => entry.category === field);
+  return result;
+
+}
 
 const list = createListOfObjects(sampleData);
-console.log(sortListOfObjectsByField(list, 'price'));
-console.log(selectByCatergory(list, 'Fruit'));
+console.log("Display list",list,"\n");
+console.log("Sorted list by field ",sortListOfObjectsByField(list, 'price'),"\n");
+console.log("Sorted through category Fruit",selectByCatergory(list, 'Fruit'),"\n");
+
+console.log("Sorted through category Stationary",selectByCatergory(list, 'Stationary'),"\n");
+
+console.log("Sorted through category Appliance",selectByCatergory(list, 'Appliance'),"\n");
+
+   
