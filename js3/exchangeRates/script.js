03af19d4ddec063bef5f3d2a3f0b876e8a4063fa
manasibendre <komalbendre6@gmail.com>
2021-03-08 11:23:48 -0500

    js3 assignments with solution

--- js3/exchangeRates/script.js
+++ js3/exchangeRates/script.js
@@ -7,7 +7,7 @@
     .then((data) => {
      console.log(" USD: ",data.rates.USD+"   INR:  "+data.rates.INR);
     
-    document.getElementById("demo").innerHTML += +"  "+ data.rates.USD +"   "+data.rates.INR+"";
+    document.getElementById("demo").innerHTML += " USD:  "+ data.rates.USD +"   INR:  "+data.rates.INR+"";
    
     })
     
