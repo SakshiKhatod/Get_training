e98a26ee49c4fd10fb4ae278046642b238e40806
Tejaswini Jadhav <tejujadhav7758@gmail.com>
2021-03-10 10:29:40 +0530

    JS5 Assignment

--- js5/fibonacci/fib.js
+++ js5/fibonacci/fib.js
@@ -1,3 +1,8 @@
-const fib = (...args) => {};
+const fib = (num) => {
+    if(num < 2){
+        return num;
+    }
+    return fib(num - 1) + fib(num - 2);
+};
 
 module.exports = fib;
--- js5/promisify/promisify.js
+++ js5/promisify/promisify.js
@@ -1,3 +1,17 @@
-const promisify = (...args) => {};
+const util = require('util');
+const promisify = (...args) => {
+    return util.promisify(...args);
+};
 
 module.exports = promisify;
+
+
+// const promise = new Promise((resolve, reject) => {
+    //     fs.readFile('./js5/promisify/data.txt', (err, data) => {
+    //         if(err) {
+    //             resolve('Failed to read file');
+    //         }
+    //         resolve(data);
+    //     });
+    // });
+    // return promise;
