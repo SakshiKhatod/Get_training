cb680cb98574e01fa102503b4bd44f95528feaef
manasibendre <komalbendre6@gmail.com>
2021-03-11 15:45:24 -0500

    js5 exercises with solution

--- js5/promisify/promisify.js
+++ js5/promisify/promisify.js
@@ -1,20 +1,14 @@
-const fs = require('fs');
-//const promisify = require('./promisify')
-const util = require('util')
-// const readFilePromise = util.promisify(fs.readFile)
-//const readFilePromise = promisify(fs.readFile);
-const validFilePath = './js5/promisify/data.txt';
-const promisify = (args) => {
-   // return util.promisify(args);
-   return new Promise((resolve, reject) => {
-     fs.readFile(args,(err, data) => {
-        if (err) return reject(err)
-        resolve(data)
-      }
-   // })
-})
-}
 
+const promisify = fn=>(...args) => {
+  return new Promise((resolve,reject) => {
+    fn(...args,(err, data) => {
+          if (err) return reject(err)
+          resolve(data)
+      })
+    }) 
+  }
+
+ 
 
 module.exports = promisify;
  
