5bc4473d154b6be76254ff663ba16e252f70cafb
manasibendre <komalbendre6@gmail.com>
2021-03-11 15:42:15 -0500

    js3 exercises with solution

--- js3/exchangeRates/script.js
+++ js3/exchangeRates/script.js
@@ -1,14 +1,28 @@
- function apiCall()
-{
-    fetch('https://api.exchangeratesapi.io/latest')
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-     console.log(" USD: ",data.rates.USD+"   INR:  "+data.rates.INR);
-    
-    document.getElementById("demo").innerHTML += " USD:  "+ data.rates.USD +"   INR:  "+data.rates.INR+"";
-   
-    })
-    
+
+//   function apiCall(){
+//    fetch('https://api.exchangeratesapi.io/latest')
+//     .then(( response ) => {
+//       return response.json();
+//     })
+//     .then(( data ) => {
+//       const Para = document.createElement("p");
+//       Para.innerHTML =+ " "+data.rates.USD + " USD " + " = "+" " + data.rates.INR+ " INR " ;
+//       document.body.appendChild(Para); 
+//       const currElement = document.getElementById('get-rate')
+//       currElement.insertAdjacentElement('afterend',Para);   
+//     })
+// }
+async function apiCall(){
+  const fetchData = await fetch('https://api.exchangeratesapi.io/latest');
+  const response = await fetchData.json();
+  return response;
 }
+function fetchApi(){
+   apiCall().then((data)=>{
+   demo.innerHTML =+ " "+data.rates.USD + " USD " + " = "+"    " + data.rates.INR+ " INR " ;
+   document.body.appendChild(demo); 
+   const currElement = document.getElementById('get-rate')
+   currElement.insertAdjacentElement('afterend',demo);  
+  })
+}
+
