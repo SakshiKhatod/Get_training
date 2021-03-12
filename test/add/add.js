2f054264dbb2e3ac2da62ffa531be1e344e5ffbb
manasibendre <komalbendre6@gmail.com>
2021-03-12 10:13:10 -0500

    tests for functions

--- test/add/add.js
+++ test/add/add.js
@@ -1,11 +1,13 @@
-function add(...numbers) {
-    const sum = numbers.reduce((acc,curr)=>{
+function add(...numbers){
+const sum = numbers.reduce((acc,curr)=>
+{
     return acc + curr;
     })
     return sum;
 }
-function mul(...numbers) {
-    const multiplication = numbers.reduce((acc,curr)=>{
+function mul(...numbers){
+const multiplication = numbers.reduce((acc,curr)=>
+{
     return acc * curr;
     })
     return multiplication;
