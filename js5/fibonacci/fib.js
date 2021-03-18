da915efd8b35fc3de9c97769ca208ea4d9ef6d2c
SakshiKhatod <sakshi.khatod1998@gmail.com>
2021-03-18 19:45:55 +0530

    Add js5 code solutions

--- js5/fibonacci/fib.js
+++ js5/fibonacci/fib.js
@@ -1,3 +1,13 @@
-const fib = (...args) => {};
+const fib = (a) => {
+
+    // return args < 1 ? 0
+    //     : args <= 2 ? 1
+    //         : fibonacci(args - 1) + fibonacci(args - 2);
+    if (a <= 1) {
+        return a;
+    }
+    return fib(a - 1) + fib(a - 2);
+};
+
 
 module.exports = fib;
--- js5/promisify/promisify.js
+++ js5/promisify/promisify.js
@@ -1,3 +1,11 @@
-const promisify = (...args) => {};
+const promisify = fn => (...args) => {
+    const promise = new Promise((resolve, reject) => {
+        fn(...args, (err, data) => {
+            if(err) reject(err);
+            resolve(data);
+        });
+    });
+    return promise;
+};
 
 module.exports = promisify;
